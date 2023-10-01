a6d0530086f8a6501d84539408e5e73fe60046c4
maixnor <46966993+maixnor@users.noreply.github.com>
2023-10-01 16:12:48 +0200

    starship in zsh, with starship.toml not yet included

--- bierbasis.nix
+++ bierbasis.nix
@@ -7,6 +7,7 @@
   home.homeDirectory = "/home/maixnor";
 
   targets.genericLinux.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -18,22 +19,34 @@
   # changes in each release.
   home.stateVersion = "23.05";
 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    LANG = "en_US.UTF-8";
+  };
+
   home.packages = [
     pkgs.zoxide
   ];
 
   programs.zsh = {
     enable = true;
-    oh-my-zsh = {
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    zplug = {
       enable = true;
-      plugins = [ "git" ];
-      # theme = "gruvbox";
+      plugins = [ 
+        { name = "zsh-users/zsh-autosuggestions"; }
+      ];
     };
   };
+
+  #TODO need to port the whole starship config here or at least integrate the file into this repo
+  programs.starship.enable = true;
 
   home.file = {
     ".local/share/zsh/nix-zsh-completions".source =
       "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
+    "starship.toml".source = "./starship.toml";
   };
 
   # Let Home Manager install and manage itself.
