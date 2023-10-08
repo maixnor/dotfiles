55ec4a059b7f3e7ba4b5c9e3a81b79aa20a486a8
maixnor <46966993+maixnor@users.noreply.github.com>
2023-10-08 19:17:02 +0200

    vim-tmux-navigator

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ...}:
+{ pkgs, lib, nix-colors, ...}:
 
 let
 in
@@ -10,11 +10,17 @@ in
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    extraConfig = ''
+      set number relativenumber
+    '';
     plugins = with pkgs.vimPlugins; [
       vim-fugitive
       vim-sensible
       nvim-lspconfig
       nvim-treesitter.withAllGrammars
+
+      vim-tmux-clipboard
+      vim-tmux-navigator
     ];
   };
 }
