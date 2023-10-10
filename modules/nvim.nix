0d8370357eea630b09bcfa176e40a3cecd518035
maixnor <46966993+maixnor@users.noreply.github.com>
2023-10-10 04:35:53 +0200

    nvim line number colors, telescope

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -20,15 +20,30 @@
 			nnoremap <leader>g :Git<cr>
 			nnoremap <leader>gp :Git push<cr>
 			nnoremap <leader>gf :Git pull<cr>
+
+			nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+			nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+			nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+			nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+			highlight LineNrAbove guifg=#${config.colorScheme.colors.base03}
+			highlight LineNr      guifg=#${config.colorScheme.colors.base04}
+			highlight LineNrBelow guifg=#${config.colorScheme.colors.base03}
     '';
     plugins = with pkgs.vimPlugins; [
+		  plenary-nvim
       vim-fugitive
       vim-sensible
       nvim-lspconfig
       nvim-treesitter.withAllGrammars
+			telescope-nvim
 
       vim-tmux-clipboard
       vim-tmux-navigator
     ];
   };
+
+	home.packages = with pkgs; [
+		ripgrep
+	];
 }
