f5bcf18781c4a675674f7d2faff66362047eed14
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2023-11-21 10:36:39 +0100

    miscelaneous services

--- modules/misc.nix
+++ modules/misc.nix
@@ -4,6 +4,31 @@
 
     home.packages = with pkgs; [
         obsidian
+				openconnect
+				gh
+				wget xh
+				freshfetch
+				spotify
+				discord
+				btop
+				pandoc
+				ripgrep
+				ollama
+
+				# kde
+				kate
+				okular
+				spectacle
+				kmail
+				konsole
+
+				# general development stuff
+				vagrant
+				fd clang coreutils just
+				jq jc jo gron yj yq pup
+
+				# unfree
+				vivaldi
     ];
 
 }
