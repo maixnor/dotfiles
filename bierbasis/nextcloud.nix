d0d4edd74f5875b11f1140a05adac858f75f72bd
maixnor <46966993+maixnor@users.noreply.github.com>
2023-12-06 06:06:55 +0100

    nextcloud with postgres, cleanup /var/lib/nextcloud if there are problems

--- bierbasis/nextcloud.nix
+++ bierbasis/nextcloud.nix
@@ -4,20 +4,29 @@
 {
 
 	environment.etc."nextcloud-admin-pass".text = "admin";
+
 	services.nextcloud = {
 	  enable = true;
 	  package = pkgs.nextcloud27;
 	  hostName = "localhost";
 		configureRedis = true;
 		maxUploadSize = "1G";
+		database.createLocally = true;
 		config = {
 	  	adminpassFile = "/etc/nextcloud-admin-pass";
 			dbtype = "pgsql";
+			defaultPhoneRegion = "AT";
 		};
 		extraApps = with config.services.nextcloud.package.packages.apps; {
     	inherit news contacts calendar tasks onlyoffice notes deck;
   	};
   	extraAppsEnable = true;
+		autoUpdateApps.enable = true;
 	};
+
+	# services.onlyoffice = {
+  #   enable = true;
+  #   hostname = "localhost";
+  # };
 
 }
