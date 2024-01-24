68deea15ef7c22e732d6c9ba6db3395702d2f235
maixnor <46966993+maixnor@users.noreply.github.com>
2024-01-24 12:56:30 +0100

    spellcheck for onlyoffice

--- modules/office.nix
+++ modules/office.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
 {
-	home.packages = [ pkgs.libreoffice-fresh ];
+	home.packages = with pkgs; [ 
+    libreoffice-fresh
+    hunspell
+    hunspellDicts.sv_SE
+    hunspellDicts.en_US-large
+    hunspellDicts.de_AT
+  ];
 }
