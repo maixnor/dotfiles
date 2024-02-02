f172edfa01f1444e752889b528e2729fe007639d
maixnor <46966993+maixnor@users.noreply.github.com>
2024-02-02 14:28:46 +0100

    numbers colors, bunch of other changes, pink comments

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -4,12 +4,12 @@
   programs.nixvim = {
     enable = true;
 
-      options = {
-    number = true;         # Show line numbers
-    relativenumber = true; # Show relative line numbers
+    options = {
+      number = true;         # Show line numbers
+      relativenumber = true; # Show relative line numbers
 
-    shiftwidth = 2;        # Tab width should be 2
-  };
+      shiftwidth = 2;        # Tab width should be 2
+    };
 
     globals.mapleader = " ";
 
@@ -19,52 +19,65 @@
       lualine.enable = true;
       bufferline.enable = true;
       fugitive.enable = true;
-      luasnip.enable = true;
       telescope.enable = true;
       tmux-navigator.enable = true;
       treesitter.enable = true;
       treesitter-textobjects.enable = true;
       undotree.enable = true;
+      nvim-colorizer.enable = true;
+      lsp-format.enable = true;
 
       lsp = {
-    enable = true;
-    servers = {
-      lua-ls.enable = true;
-      rust-analyzer.enable = true;
-      nixd.enable = true;
-    };
+	enable = true;
+	servers = {
+	  lua-ls.enable = true;
+	  rust-analyzer.enable = true;
+	  nixd.enable = true;
+	};
       };
     };
 
-    plugins.lsp.servers.rust-analyzer.installCargo = false;
-    plugins.lsp.servers.rust-analyzer.installRustc = false;
+    plugins.lsp.servers.rust-analyzer.installCargo = true;
+    plugins.lsp.servers.rust-analyzer.installRustc = true;
+
+    highlight = {
+      Comment.fg = "#f47ac9";
+      Comment.underline = true;
+      Comment.bold = true;
+      Normal.bg = "NONE";
+      NonText.bg = "NONE";
+
+      LineNrAbove.fg="#${config.colorScheme.palette.base03}";
+      LineNr.fg="#${config.colorScheme.palette.base04}";
+      LineNrBelow.fg="#${config.colorScheme.palette.base03}";
+    };
 
     keymaps = [
-    {
-      action = "<cmd>Telescope live_grep<CR>";
-      key = "<leader>fg";
-    }
-    {
-      action = "<cmd>Telescope find_files<CR>";
-      key = "<leader>ff";
-    }
-    {
-      action = "<cmd>Git<CR>";
-      key = "<leader>gp";
-    }
-    {
-      action = "<cmd>Git push<CR>";
-      key = "<leader>gp";
-    }
-    {
-      action = "<cmd>Git pull<CR>";
-      key = "<leader>gf";
-    }
-    {
-      action = "<cmd>UndoTreeShow<CR>";
-      key = "<leader>u";
-    }
-  ];
+      {
+	action = "<cmd>Telescope live_grep<CR>";
+	key = "<leader>fg";
+      }
+      {
+	action = "<cmd>Telescope find_files<CR>";
+	key = "<leader>ff";
+      }
+      {
+	action = "<cmd>Git<CR>";
+	key = "<leader>g";
+      }
+      {
+	action = "<cmd>Git push<CR>";
+	key = "<leader>gp";
+      }
+      {
+	action = "<cmd>Git pull<CR>";
+	key = "<leader>gf";
+      }
+      {
+	action = "<cmd>UndoTreeShow<CR>";
+	key = "<leader>u";
+      }
+    ];
 
     plugins.nvim-cmp = {
       enable = true;
@@ -73,7 +86,6 @@
         {name = "nvim_lsp";}
         {name = "path";}
         {name = "buffer";}
-        {name = "luasnip";}
       ];
 
       mapping = {
@@ -83,10 +95,6 @@
             function(fallback)
               if cmp.visible() then
                 cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
               elseif check_backspace() then
                 fallback()
               else
