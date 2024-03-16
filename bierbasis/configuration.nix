7fcecb7448ec5f58970dc30187e09b181d1f0c9c
maixnor <46966993+maixnor@users.noreply.github.com>
2024-03-16 05:49:13 +0100

    bierbasis plasma 6

--- bierbasis/configuration.nix
+++ bierbasis/configuration.nix
@@ -61,9 +61,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-	# launch in wayland session per default
-	services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
