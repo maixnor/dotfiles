9a40e34b1a6564e73be06a8bbbfd4bebe07795af
maixnor <46966993+maixnor@users.noreply.github.com>
2024-09-05 12:01:52 +0200

    grub bootloader

--- wieselburg/configuration.nix
+++ wieselburg/configuration.nix
@@ -4,32 +4,41 @@
   imports = [ 
     ./hardware-configuration.nix
     inputs.nixvim.nixosModules.nixvim
-
     ../modules/nixvim.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
 
+  ### Bootloader Configuration
+  boot.loader.grub.enable = true;  # Enables GRUB bootloader
+  boot.loader.grub.version = 2;    # Specifies GRUB version
+  boot.loader.grub.devices = [ "/dev/sda" ];  # Install GRUB to /dev/sda
+  boot.loader.grub.useOSProber = true;        # Enables OS prober if multi-booting
+
+  ### FileSystem Configuration
   fileSystems."/mnt/mass" = {
     device = "/dev/sda";
     fsType = "ext4";
   };
 
+  ### Networking Configuration
   networking.useDHCP = false;
   networking.defaultGateway = "10.0.30.1";
   networking.interfaces.ens32.ipv4.addresses = [{
-   address = "10.0.30.13";
-   prefixLength = 24;
+    address = "10.0.30.13";
+    prefixLength = 24;
   }];
 
+  ### User Configuration
   users.users.maixnor = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     packages = with pkgs; [ gh ];
   };
 
+  ### Services Configuration
   services.openssh.enable = true;  
-  services.zerotierone = { enable = true; joinNetworks = ["8056C2E21CF844AA"];};
+  services.zerotierone = { enable = true; joinNetworks = ["8056C2E21CF844AA"]; };
 
   services.searx.enable = true;
   services.searx.settings = {
@@ -38,17 +47,18 @@
     server.secret_key = "definetelysecret";
   };
 
-  # actual services
+  ### System Packages
   environment.systemPackages = with pkgs; [ 
     git 
     wormhole-william
     appflowy
   ];
+
+  # Uncomment and configure these services if needed
   # services.jitsi-meet = { enable = true; hostName = "gehinoasch"; };
   # services.mattermost = { enable = true; };
   # services.nextcloud = { enable = true; hostName = "gehinoasch"; };
 
-  # let's encrypt EULA
+  ### Let's Encrypt (ACME) Configuration
   security.acme.acceptTerms = true;
-
 }
