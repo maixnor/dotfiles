a7c4a5bd6285716b4d4b85d93c638e248e86c140
maixnor <46966993+maixnor@users.noreply.github.com>
2025-06-19 05:37:21 +0200

    use correct zerotier networks

--- modules/zerotier.nix
+++ modules/zerotier.nix
@@ -7,9 +7,7 @@
     services.zerotierone = { 
       enable = true; 
       joinNetworks = [ 
-        "8056C2E21CF844AA" 
         "856127940c7eb96b" 
-        "856127940c5dae71" 
       ]; 
     };
   };
