e51d9a4f72706c8bbe8d978f2cfef23be7097e58
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2025-07-08 23:10:02 +0200

    autoupdate compiles

--- services/autoupdate.nix
+++ services/autoupdate.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 with lib;
 let 
@@ -16,6 +16,14 @@ in
   };
 
   config = mkIf cfg.enable { 
+    systemd.timers.autoupdate-timer = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*:0/5"; # every 5 minutes
+        Unit = "autoupdate.service";
+      };
+    };
+
     systemd.services.autoupdate = {
       description = "Update system to latest state on GitHub";
       path = with pkgs; [ git just ];
@@ -26,6 +34,7 @@ in
           if git fetch origin main && ! git diff --quiet DEAD..origin/main; then
             git pull origin main
             just ${cfg.hostname}
+          fi
         '';
       };
     };
