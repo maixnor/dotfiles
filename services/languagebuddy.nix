1e8e1f9edebb5f762c20a86ef7467c3ffc70d16e
maixnor <46966993+maixnor@users.noreply.github.com>
2025-10-08 10:19:45 -0500

    permissions,otra vez

--- services/languagebuddy.nix
+++ services/languagebuddy.nix
@@ -11,6 +11,9 @@ in
     "d /var/www/languagebuddy 0755 maixnor maixnor -"
     "d /var/www/languagebuddy/prod 0755 maixnor maixnor -"
     "d /var/www/languagebuddy/test 0755 maixnor maixnor -"
+    # Recursively fix ownership and permissions for deployment
+    "Z /var/www 0755 maixnor maixnor -"
+    "Z /var/www/languagebuddy 0755 maixnor maixnor -"
     "Z /var/www/languagebuddy/prod 0755 maixnor maixnor -"
     "Z /var/www/languagebuddy/test 0755 maixnor maixnor -"
   ];
