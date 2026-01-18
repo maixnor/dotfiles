ed909482039a907daf2fd59bc7c7a4ed4329993e
maixnor <46966993+maixnor@users.noreply.github.com>
2026-01-18 15:58:18 +0100

    fix: environment paths for autoupdate and resolve dns warning

--- wieselburg/vpsadminos.nix
+++ wieselburg/vpsadminos.nix
@@ -15,7 +15,7 @@ let
   ];
 in {
   networking.nameservers = mkDefault nameservers;
-  services.resolved = mkDefault { fallbackDns = nameservers; };
+  services.resolved.settings.Resolve.FallbackDNS = mkDefault (concatStringsSep " " nameservers);
   networking.dhcpcd.extraConfig = "noipv4ll";
 
   systemd.services.systemd-sysctl.enable = false;
