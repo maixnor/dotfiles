9782b883e9f8ae07f6d3eafc9723b2c0b3e56a56
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2026-07-25 07:03:19 +0200

    bierland for the win

--- modules/zerotier.nix
+++ modules/zerotier.nix
@@ -8,6 +8,7 @@
       enable = true; 
       joinNetworks = [ 
         #"856127940c7eb96b" 
+        "0cccb752f76d2546" # BIERLAND
         "e3918db483a80e0b"
         "b103a835d2f0706f"
         "8056c2e21c546100"
